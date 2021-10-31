6d8fceff411a02171c646b7655c61c7e59504c3e
hmaravlad <hmara.vladislav2@gmail.com>
2021-10-31 20:38:57 +0200

    make date more recent

--- src/generator/blocks/recent-date.ts
+++ src/generator/blocks/recent-date.ts
@@ -6,7 +6,7 @@ export class RecentDateBlock implements Block {
   type = BlockType.Number;
 
   generate(): string {
-    const year = getRandomInt(1900, 2022).toString();
+    const year = getRandomInt(1970, 2022).toString();
     const month = getRandomInt(1, 13).toString().padStart(2, '0');
     const day = getRandomInt(1, 32).toString().padStart(2, '0');
     return day + month + year;
